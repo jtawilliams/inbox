329cacc798d9f47bf9583fe10c0ccaf15d363560
jtawilliams <jtawilliams@yahoo.com>
2018-11-06 15:08:33 -0700

    MessageList and single message working

---
+++ src/components/MessageList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+import Message from './Message'
+
+let MessageList = (props) => {
+  return (
+    <Message></Message>
+  )
+}
+
+export default MessageList;
